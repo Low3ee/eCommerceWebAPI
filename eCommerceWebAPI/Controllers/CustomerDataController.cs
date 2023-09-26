b553ee9f74f257b2514dc5d9b36565b85fbf4908
Low3ee <louieabad000@gmail.com>
2023-09-27 01:39:54 +0800

    Add project files.

---
+++ eCommerceWebAPI/Controllers/CustomerDataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerDataController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<CustomerData>>> GetCustomerData()
+        {
+            return new List<CustomerData>
+            {
+                new CustomerData
+                {
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "jDoe69@gmail.com"
+                }
+            };
+        }
+    }
+}
---
+++ eCommerceWebAPI/CustomerData.cs
@@ -0,0 +1,10 @@
+﻿namespace eCommerceWebAPI
+{
+    public class CustomerData
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
