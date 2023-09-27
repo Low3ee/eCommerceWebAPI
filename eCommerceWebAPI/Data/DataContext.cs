14174c79ac6fbc3d3acb1bc120ea20475e0d3636
Low3ee <louieabad000@gmail.com>
2023-09-27 14:31:06 +0800

    database connection from API

---
+++ eCommerceWebAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceWebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
+
+        public DbSet<CustomerData> Customers => Set <CustomerData>();
+    }
+}
